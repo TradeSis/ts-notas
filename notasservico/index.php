e6ef49c200a728507204bd2b90e34465b9e1f7d0
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2023-10-13 17:00:31 -0300

    alterado classe titloTabela para ts-tituloPrincipal

--- notasservico/index.php
+++ notasservico/index.php
@@ -69,7 +69,7 @@ $clientes = buscaClientes();
             </div>
 
             <div class="col-12 col-sm-12 col-md-12 col-lg-2 order-lg-2 mt-4">
-                <h2 class="tituloTabela">Notas Serviço</h2>
+                <h2 class="ts-tituloPrincipal">Notas Serviço</h2>
             </div>
 
             <div class="col-12 col-sm-12 col-md-12 col-lg-5 order-lg-3">
@@ -292,6 +292,8 @@ $clientes = buscaClientes();
     <!-- LOCAL PARA COLOCAR OS JS -->
 
     <?php include_once ROOT . "/vendor/footer_js.php"; ?>
+    <!-- script para menu de filtros -->
+    <script src= "<?php echo URLROOT ?>/sistema/js/filtroTabela.js"></script>
 
     <script>
         buscar($("#FiltroClientes").val(), $("#buscanotas").val(), $("#FiltroStatusNota").val());
@@ -416,15 +418,7 @@ $clientes = buscaClientes();
             });
         });
 
-       /*  $('.btnAbre').click(function() {
-            $('.menuFiltros').toggleClass('mostra');
-            $('.diviFrame').toggleClass('mostra');
-        }); */
-        /* Novo script para menu filtros */
-        $('.ts-btnFiltros').click(function() {
-            $('.ts-menuFiltros').toggleClass('mostra');
-            $('.ts-tableFiltros').toggleClass('mostra');
-        });
+   
 
 
         var inserirModal = document.getElementById("inserirModal");
